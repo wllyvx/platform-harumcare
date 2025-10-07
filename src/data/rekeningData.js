5806b496ad1d19b8b5a0bcf50f24ca6074ae7c5a
wllyvx <willyfr88@gmail.com>
2025-10-07 07:35:03 +0700

    adding rekening bank to payment page

---
+++ src/data/rekeningData.js
@@ -0,0 +1,26 @@
+// rekening.js - Data dummy untuk informasi rekening bank, termasuk data tambahan dari transfer
+
+const rekeningData = [
+    {
+      namaBank: 'BJB Syariah',
+      noRekening: '5150201014901',
+      atasNama: 'Bidang Dakwah dan Sosial HARUM'
+    },
+    {
+      namaBank: 'Bank BSI',
+      noRekening: '7189282774',
+      atasNama: 'An Dakwah Dan Sosial Harapan Umat'
+    },
+    {
+      namaBank: 'Bank BRI',
+      noRekening: '0116101070566',
+      atasNama: 'PUB Yayasasan Dakwah Dan Sosial Harapan Umamat'
+    },
+    {
+      namaBank: 'Bank BRI',
+      noRekening: '341002354',
+      atasNama: 'Sosial Harapan Umat Karawang YYS'
+    }
+  ];
+  
+  export default rekeningData;
